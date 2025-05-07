40e4fe62b8cc81673f097a571d7a79efdba2a6da
Matthew Galarza <mgalarza.rpi@gmail.com>
2025-05-06 22:18:18 -0400

    Update collision_model.jl

--- collision_model.jl
+++ collision_model.jl
@@ -41,7 +41,6 @@ export Params, p, electrostatic, CoupledSystem!
     e::T = 8.85e-12          # Permittivity of free space (F/m)
     ep::T = 3.2              # Permittivity of parylene
     Vbias::T = 3.0           # Bias voltage (V)
-
     Rload::T = 0.42e6        # Load resistance (Ω)
     N::Int = 160             # Number of electrodes
     kss::T = 6.0             # Soft-stopper spring force (N/m)
